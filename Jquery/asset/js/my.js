1bc8238ac9f0843f88431d2c113d1d5e051afd47
FatemehMotamed <motamed.258@gmail.com>
2020-11-10 19:04:55 +0330

    jquery start

---
+++ Jquery/asset/js/my.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#dangerButton').click(function(){
+        $("#dangerText").fadeOut(1000);
+    });
+    
+    $('.selectButton').click(function(){
+        $(".selectText").fadeOut(1000);
+    });
+    
+});
